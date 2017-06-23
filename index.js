7258f0479c3e36be99bfd2cd357916734d08f30c
Jason Tuttle <jason@hearspotbark.com>
2017-06-23 15:38:49 -0400

    partially functional hardmode 1

--- index.js
+++ index.js
@@ -27,13 +27,42 @@ app.use('/resources', express.static(path.join(__dirname, './public')));
 app.use('/', express.static(path.join(__dirname, './')));
 app.use(bodyParser.urlencoded({extended: true}));
 
+const pickWord = function (array, mode) {
+  let min, max;
+  switch(mode) {
+    case "easy":
+      min = 4;
+      max = 6;
+      break;
+    case "normal":
+      min = 6;
+      max = 8;
+      break;
+    case "hard":
+      min = 8;
+      max = 25;
+      break;
+    default:
+      break;
+  }
+  do {
+    let word = array[(Math.floor(Math.random() * array.length))];
+  } while ((word.length < min) && (word.length > max));
+  return word;
+};
+
+app.post('/', function(req, res) {
+  req.session.mode = req.body.game_mode;
+  res.redirect('/game');
+})
+
 //fetch the word dictionary, grab a random word and store it in the session
 app.use('/game', function(req, res, next) {
   var game = req.session.game;
   if (!game) {
     game = req.session.game = {};
     const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-    game.word = words[(Math.floor(Math.random() * words.length))];
+    game.word = pickWord(words, req.session.game_mode);
     game.guessWord = Array.from(game.word).fill("_");
     game.guessed = [];
     game.guessesLeft = 8;
@@ -53,27 +82,28 @@ app.post('/game', function(req, res) {
     .isAlpha();
   const errors = req.validationErrors();
 
-  // lets use a shorter name for everything
+  // lets use shorter names for everything
   const game = req.session.game;
+  const guess = req.body.guess.toLowerCase();
 
   if (game.guessesLeft > 1) {
     if (errors) {  // check for form errors
       res.render('game', { game: game, error: errors[0].msg })
     } else {  // check for valid but repeated guesses
-      if (game.guessed.includes(req.body.guess)) {
+      if (game.guessed.includes(guess)) {
         res.render('game', { game: game, error: "You've already guessed that letter" });
       } else {  // no errors, no repeats
-        if (!game.word.includes(req.body.guess)) { // letter not in the word to guess
+        if (!game.word.includes(guess)) { // letter not in the word to guess
           game.guessesLeft--;
-          game.guessed.push(req.body.guess);
+          game.guessed.push(guess);
           res.render('game', { game: game });
         } else { // word contains our guess
           game.word.split('').forEach(function(letter, idx) { // replace blanks with the matches
-            if (letter === req.body.guess) {
-              game.guessWord[idx] = req.body.guess;
+            if (letter === guess) {
+              game.guessWord[idx] = guess;
             }
           });
-          game.guessed.push(req.body.guess);
+          game.guessed.push(guess);
           if (!game.guessWord.includes("_")) {  // if there are no more blanks in the guessWord...
             res.render('game', { game: game, win: true, winLossMessage: "You win! 😁"})
           } else {  // otherwise we have more letters to guess
@@ -86,6 +116,11 @@ app.post('/game', function(req, res) {
     game.guessesLeft--;
     res.render('game', { game: game, lose: true, winLossMessage: "You're out of guesses... ☹️"})
   }
+});
+
+app.post('/reset', function(req, res) {
+  req.session.game = null;
+  res.redirect('/game');
 });
 
 app.listen(3030, () => console.log("Roll tape, quiet in the house..."));
