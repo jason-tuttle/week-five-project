ae45939771074013db382369827c96dc220d3973
Jason Tuttle <jason@hearspotbark.com>
2017-06-23 13:31:25 -0400

    functional game so far, no style

--- index.js
+++ index.js
@@ -51,8 +51,51 @@ app.post('/game', function(req, res) {
     .notEmpty()
     .isLength(1)
     .isAlpha();
-    
+  const errors = req.validationErrors();
 
+  // lets use a shorter name for everything
+  const game = req.session.game;
+
+  if (game.guessesLeft > 1) {
+    if (errors) {  // check for form errors
+      res.render('game', { game: game, error: errors[0].msg })
+    } else {  // check for valid but repeated guesses
+      if (game.guessed.includes(req.body.guess)) {
+        res.render('game', { game: game, error: "You've already guessed that letter" });
+      } else {  // no errors, no repeats
+        if (!game.word.includes(req.body.guess)) { // letter not in the word to guess
+          game.guessesLeft--;
+          game.guessed.push(req.body.guess);
+          res.render('game', { game: game });
+        } else { // word contains our guess
+          game.word.split('').forEach(function(letter, idx) { // replace blanks with the matches
+            if (letter === req.body.guess) {
+              game.guessWord[idx] = req.body.guess;
+            }
+          });
+          game.guessed.push(req.body.guess);
+          if (!game.guessWord.includes("_")) {  // if there are no more blanks in the guessWord...
+            res.render('game', { game: game, win: true, winLossMessage: "You win! 😁"})
+          } else {  // otherwise we have more letters to guess
+            res.render('game', { game: game });
+          }
+        }
+      }
+    }
+  } else { // we just used our last guess
+    game.guessesLeft--;
+    res.render('game', { game: game, lose: true, winLossMessage: "You're out of guesses... ☹️"})
+  }
 });
 
 app.listen(3030, () => console.log("Roll tape, quiet in the house..."));
+
+/*
+☹️
+frowning face
+Unicode: U+2639 U+FE0F, UTF-8: E2 98 B9 EF B8 8F
+
+😁
+grinning face with smiling eyes
+Unicode: U+1F601, UTF-8: F0 9F 98 81
+*/
